8d04cc46e0857497cc1e4a2b0404b72da9cbd921
samuel <samuelufop121035@gmail.com>
2023-07-28 12:03:13 -0300

    final version with cloud backend request

--- src/services/books.js
+++ src/services/books.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const booksAPI = axios.create({baseURL:"http://35.247.212.57:8000/books"})
+const booksAPI = axios.create({baseURL:"https://35.247.212.57:8000/books"})
 
  async function getBooks() {
     const response = await booksAPI.get('/')
